219dc3f9bd4f62bae35d0e3d01765c8ae3b5d839
Maximus1641 <ahmedhaidara4@gmail.com>
2017-10-18 09:07:17 +0000

    stable 1.0
    Mise en place de leading page

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,9 @@
-class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
+class UserMailer < ApplicationMailer
+  default from: 'notifications@example.com'
+ 
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
 end
